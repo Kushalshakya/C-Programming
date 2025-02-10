8d4fcdf606174fc45859ef16a3f87c2e5cd195a8
Kushal Shakya <kushalshakya4114@gmail.com>
2025-02-10 20:23:30 +0545

    Structur Basics

---
+++ Structures/StructIntro.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+struct student{
+    int rollno;
+    char name[20];
+    char address[30];
+};
+
+int main(){
+    struct student s[5];
+    int i;
+    printf("Enter Rollno, Name and Address of the Student: ");
+    for(i=0; i<2; i++){
+        scanf("%d%s%s", &s[i].rollno, s[i].name, s[i].address);
+    }
+
+    for(i=0; i<2; i++){
+        printf("RollNo: %d\tName: %s\tAddress: %s\n", s[i].rollno, s[i].name, s[i].address);
+    }
+
+    return 0;
+}
